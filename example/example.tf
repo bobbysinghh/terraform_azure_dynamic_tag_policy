9e124dd5f88f12eea4fe9cf1f9d8318116ea6c9c
Bobby Singh <Bharat8249@gmail.com>
2022-01-10 14:14:41 +0530

    initial commit

--- example/example.tf
+++ example/example.tf
@@ -2,12 +2,12 @@ provider "azurerm" {
   features {}
 }
 module "tag_policy" {
-  source = "../"
-  policy_name          = "appServiceMSI"
-  policy_mode          = "All"
-  policy_display_name  = "App Service"
-  not_scopes           = null
-  location             = null
+  source              = "../"
+  policy_name         = "appServiceMSI"
+  policy_mode         = "All"
+  policy_display_name = "App Service"
+  not_scopes          = null
+  location            = null
   required_tags = {
     department = {
       tagName         = "department"
